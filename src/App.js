31add5fb729bb048c6d57ff05e9d127f0af397f4
DESKTOP-RB42N5K\Maji <Maede.alirezaei@gmail.com>
2021-11-27 15:41:01 +0330

    edit app

--- src/App.js
+++ src/App.js
@@ -1,32 +1,7 @@
-import { Fragment, useCallback, useEffect, useState } from "react";
+import { Fragment } from "react";
 import Body from "./components/Body";
 import Header from "./components/Header";
 function App() {
-  const [error, setError] = useState(null);
-  const [isLoading, setIsLoading] = useState(false);
-  const [examInfo, setExamInfo] = useState(null);
-
-  const fetchHandler = useCallback(async () => {
-    setError(null);
-    setIsLoading(true);
-
-    try {
-      const response = await fetch(`http://localhost:5000/exam/info`);
-      if (!response.ok) {
-        throw new Error();
-      }
-      const examData = await response.json();
-      setExamInfo(examData);
-
-      setIsLoading(false);
-    } catch (error) {
-      setError(true);
-    }
-  }, []);
-
-  useEffect(() => {
-    fetchHandler();
-  }, [fetchHandler]);
   
   return (
     <Fragment>
